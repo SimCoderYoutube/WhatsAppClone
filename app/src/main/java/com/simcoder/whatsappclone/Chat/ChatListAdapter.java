3b142238852667aca176e0d3afabdf7a5f659497
SimCoder <simplecodervideos@gmail.com>
2018-08-15 12:52:19 +0100

    Lesson9-SendAMessage

--- app/src/main/java/com/simcoder/whatsappclone/Chat/ChatListAdapter.java
+++ app/src/main/java/com/simcoder/whatsappclone/Chat/ChatListAdapter.java
@@ -1,6 +1,8 @@
 package com.simcoder.whatsappclone.Chat;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,16 +13,17 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.simcoder.whatsappclone.ChatActivity;
 import com.simcoder.whatsappclone.R;
 
 import java.util.ArrayList;
 
 public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder> {
 
-    ArrayList<ChatObject> ChatList;
+    ArrayList<ChatObject> chatList;
 
-    public ChatListAdapter(ArrayList<ChatObject> ChatList){
-        this.ChatList = ChatList;
+    public ChatListAdapter(ArrayList<ChatObject> chatList){
+        this.chatList = chatList;
     }
 
     @NonNull
@@ -35,19 +38,24 @@ public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatLi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ChatListViewHolder holder, final int position) {
-        holder.mTitle.setText(ChatList.get(position).getChatId());
+    public void onBindViewHolder(@NonNull final ChatListViewHolder holder, final int position) {
+        holder.mTitle.setText(chatList.get(position).getChatId());
 
         holder.mLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ChatActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("chatID", chatList.get(holder.getAdapterPosition()).getChatId());
+                intent.putExtras(bundle);
+                v.getContext().startActivity(intent);
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return ChatList.size();
+        return chatList.size();
     }
 
 
--- app/src/main/java/com/simcoder/whatsappclone/ChatActivity.java
+++ app/src/main/java/com/simcoder/whatsappclone/ChatActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.simcoder.whatsappclone.Chat.ChatObject;
 import com.simcoder.whatsappclone.Chat.MessageAdapter;
 import com.simcoder.whatsappclone.Chat.MessageObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -20,19 +28,46 @@ public class ChatActivity extends AppCompatActivity {
 
     ArrayList<MessageObject> messageList;
 
+    String chatID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        chatID = getIntent().getExtras().getString("chatID");
+
+        Button mSend = findViewById(R.id.send);
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
         initializeRecyclerView();
+    }
+
+    private void sendMessage(){
+        EditText mMessage = findViewById(R.id.message);
+
+        if(!mMessage.getText().toString().isEmpty()){
+            DatabaseReference newMessageDb = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+
+            Map newMessageMap = new HashMap<>();
+            newMessageMap.put("text", mMessage.getText().toString());
+            newMessageMap.put("creator", FirebaseAuth.getInstance().getUid());
+
+            newMessageDb.updateChildren(newMessageMap);
+        }
+        mMessage.setText(null);
     }
 
 
 
     private void initializeRecyclerView() {
         messageList = new ArrayList<>();
-        mChat= findViewById(R.id.message);
+        mChat= findViewById(R.id.messageList);
         mChat.setNestedScrollingEnabled(false);
         mChat.setHasFixedSize(false);
         mChatLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL, false);
